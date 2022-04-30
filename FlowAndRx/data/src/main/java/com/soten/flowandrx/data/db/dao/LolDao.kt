b9d93514451f29e08a033b6a090c91c99a29a5d1
Choiyounho <maxosa78@gmail.com>
2022-04-30 19:16:04 +0900

    feat : 검색 쿼리 구현

--- FlowAndRx/data/src/main/java/com/soten/flowandrx/data/db/dao/LolDao.kt
+++ FlowAndRx/data/src/main/java/com/soten/flowandrx/data/db/dao/LolDao.kt
@@ -13,6 +13,9 @@ interface LolDao {
     @Query("SELECT * FROM LolChampionInfo")
     fun getAllLolChampions(): Flow<List<LolChampionInfo>>
 
+    @Query("SELECT * FROM LolChampionInfo WHERE name LIKE :query")
+    fun search(query: String): Flow<List<LolChampionInfo>>
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertLolChampions(championInfo: LolChampionInfo)
 

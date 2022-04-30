ab62952a06d07a2d994176427216f9a24d813125
Choiyounho <maxosa78@gmail.com>
2022-04-30 18:05:20 +0900

    feat : Dao 기능 추가

--- FlowAndRx/data/src/main/java/com/soten/flowandrx/data/db/dao/LolDao.kt
+++ FlowAndRx/data/src/main/java/com/soten/flowandrx/data/db/dao/LolDao.kt
@@ -1,6 +1,8 @@
 package com.soten.flowandrx.data.db.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.soten.flowandrx.data.db.entity.LolChampionInfo
 import kotlinx.coroutines.flow.Flow
@@ -10,5 +12,8 @@ interface LolDao {
 
     @Query("SELECT * FROM LolChampionInfo")
     fun getAllLolChampions(): Flow<List<LolChampionInfo>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertLolChampions(championInfo: LolChampionInfo)
 
 }

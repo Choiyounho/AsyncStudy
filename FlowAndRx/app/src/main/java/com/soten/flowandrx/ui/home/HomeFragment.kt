a5b2e1dc890048080261582cc909382d0065e5b0
Choiyounho <maxosa78@gmail.com>
2022-04-30 19:16:31 +0900

    feat : 홈 화면 기능 구현

--- FlowAndRx/app/src/main/java/com/soten/flowandrx/ui/home/HomeFragment.kt
+++ FlowAndRx/app/src/main/java/com/soten/flowandrx/ui/home/HomeFragment.kt
@@ -1,13 +1,19 @@
 package com.soten.flowandrx.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.soten.flowandrx.databinding.FragmentHomeBinding
+import com.soten.flowandrx.ui.home.adpater.ChampionAdapter
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
@@ -16,6 +22,8 @@ class HomeFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val viewModel by viewModels<HomeViewModel>()
+
+    private val adapter = ChampionAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,7 +37,34 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.championRecyclerView.adapter = adapter
 
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.allChampions.collectLatest {
+                adapter.submitList(it)
+            }
+        }
+
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                newText?.let {
+                    viewLifecycleOwner.lifecycleScope.launch {
+                        viewModel.searchChampions("%$it%").collectLatest {
+                            Log.d("TestT", "$it")
+                            adapter.submitList(it)
+                        }
+                    }
+                }
+                return true
+            }
+        })
+        viewLifecycleOwner.lifecycleScope.launch {
+
+        }
     }
 
     override fun onDestroyView() {
@@ -37,3 +72,4 @@ class HomeFragment : Fragment() {
         _binding = null
     }
 }
+
